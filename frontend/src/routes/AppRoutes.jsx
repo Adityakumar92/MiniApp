c4387abcdcfa69ee199aa16eceae88c0821d998d
Adityakumar92 <little.aditya.47@gmail.com>
2025-10-22 20:10:17 +0530

    bug fix

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import PrivateRoute from "./PrivateRoute";
